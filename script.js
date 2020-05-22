fd4e9c93227d5f05d32b170288b44d2617ba98de
Sergey <53293773+SergeyLavrentyev@users.noreply.github.com>
2020-05-22 22:10:53 +0400

    init

---
+++ script.js
@@ -0,0 +1,16 @@
+// #1 assignment
+
+let age, firstName, lastNamem, user
+
+age = +prompt("How old are you?: ", 18);
+firstName = prompt("What's your name?: ");
+lastName = prompt("What's your last name?: ");
+
+
+user = {
+  age: age,
+  firstName: firstName,
+  lastName: lastName,
+}
+
+console.log(user);
