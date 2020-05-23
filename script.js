1cf8ee3538933edc9d53f4701b3a972d3a20519c
Sergey <53293773+SergeyLavrentyev@users.noreply.github.com>
2020-05-23 14:24:41 +0400

    2hometask

--- script.js
+++ script.js
@@ -14,3 +14,48 @@ user = {
 }
 
 console.log(user);
+
+// #2a assignment
+function giveNumber() {
+
+  let num = +prompt("Write a number from 1 to 10: "),
+    i = 0;
+  if (num < 1) {
+    alert(`${num} is less than 1. Please try again`);
+    giveNumber();
+  } else if (num > 10) {
+    alert(`${num} is bigger than 10. Please try again`);
+    giveNumber();
+  } else if (Number.isNaN(num)) {
+    alert(`It is not a number. Try again!`);
+    giveNumber();
+  } else {
+    for (i = 0; i <= num; i++){
+      if (i % 4 !== 0) {
+        console.log(i);
+      }
+    }
+  }
+}
+
+giveNumber();
+
+// #2b assignment
+function count() {
+  
+  let timerId = setInterval(function() {
+    if (number >= 0) {
+      number += 3;
+      console.log(number);
+    }
+  }, 3000);
+  
+  setTimeout(function () {
+    clearInterval(timerId);
+    alert("Programm is over")
+  }, 9000)
+}
+
+let number = 0;
+
+count();
