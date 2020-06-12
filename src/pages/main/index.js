9c90f1c7ec9632e4046ad1d38d05e206b43e2555
Sergey <53293773+SergeyLavrentyev@users.noreply.github.com>
2020-06-12 21:38:51 +0400

    form, button0to-top, slider(not finished)

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,3 +1,40 @@
+var signIn = 'sign-in';
+var register = 'register';
+var sendMessage = 'message';
+
+function modalWindow(blockName, formNumber) {
+  var blockName;
+  var formNumber;
+  const openButton = document.querySelector(`.${blockName}--js`);
+  const modalForm = document.querySelector(`.modal-window__${blockName}--js`);
+  const formClose = document.querySelectorAll('.modal-window__close--js')[`${formNumber}`];
+  const input = document.querySelector(`.${blockName}__input`);
+  const body = document.querySelector('body');
+  
+  openButton.addEventListener('click', function () {
+    modalForm.classList.remove('modal-window--hidden');
+    input.focus();
+    body.style.overflowY = 'hidden';
+  });
+
+  formClose.addEventListener('click', function () {
+    modalForm.classList.add('modal-window--hidden');
+    openButton.focus();
+    body.style.overflowY = 'scroll';
+  });
+
+  window.addEventListener('keydown', function (event) {
+    if (!modalForm.classList.contains('modal-window--hidden') && event.code === 'Escape') {
+      modalForm.classList.add('modal-window--hidden');
+      openButton.focus();
+    }
+  });
+}
+
+modalWindow(signIn, 0);
+modalWindow(register, 1);
+modalWindow(sendMessage, 2);
+
 (function () {
   const toTop = document.querySelector('.arrow--top_js')
 
@@ -16,4 +53,5 @@
       behavior: 'smooth'
     })
   })
-})()
+})();
+
